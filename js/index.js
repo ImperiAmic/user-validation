7ff9e8d413dda1ee0a346d0e1cdc9ebb8ee9c72f
Raimon <raimon.txollo@gmail.com>
2025-02-09 13:00:24 +0100

    Edit from Mario's comments

--- js/index.js
+++ js/index.js
@@ -1,30 +1,33 @@
-const userName = "Raimon";
+const username = "Raimon";
 const password = "Dué:0y.^*!Xn>7DFh.Ò?#@3W";
-const passwordRepeat = "Dué:0y.^*!Xn>7DFh.Ò?#@3W";
+const repeatedPassword = "Dué:0y.^*!Xn>7DFh.Ò?#@3W";
 const age = 34;
 
-const regexString = /\w{5,}/g;
-const nameValidation = regexString.test(userName);
+const usernameValidationRegex = /\w{5,}/g;
+const isUsernameValid = usernameValidationRegex.test(username);
 
+const passwordMinimumLength = 8;
 const passwordLength = password.length;
-const passwordLengthValidation = passwordLength >= 8;
+const isPasswordLengthValid = passwordLength >= passwordMinimumLength;
 
-const regexNumber = /\d{1,}/g;
-const passwordNumberValidation = regexNumber.test(password);
+const passwordNumberValidationRegex = /\d{1,}/g;
+const hasPasswordAtLeastOneNumber =
+  passwordNumberValidationRegex.test(password);
 
-const regexMayus = /[A-ZÀ-Ú]/g;
-const passwordLetterMayusValidation = regexMayus.test(password);
+const passwordUpperCaseRegex = /[A-ZÀ-Ú]/g;
+const hasPasswordAtLeastOneUpperCaseLetter =
+  passwordUpperCaseRegex.test(password);
 
-const passwordValidation =
-  (passwordLengthValidation === passwordNumberValidation) ===
-  passwordLetterMayusValidation;
+const isPasswordValid =
+  (isPasswordLengthValid === hasPasswordAtLeastOneNumber) ===
+  hasPasswordAtLeastOneUpperCaseLetter;
 
-const passwordRepeatValidation = passwordRepeat.includes(password);
+const isPasswordRepeatedValid = repeatedPassword.includes(password);
 
 const adultAge = 18;
-const ageValidation = age >= adultAge;
+const isAgeValid = age >= adultAge;
 
-console.log(`El nombre de usuario es válido: ${nameValidation}
-La contraseña es válida: ${passwordValidation}
-La repetición de la contraseña es válida: ${passwordRepeatValidation}
-Es mayor de edad: ${ageValidation}`);
+console.log(`El nombre de usuario es válido: ${isUsernameValid}
+La contraseña es válida: ${isPasswordValid}
+La repetición de la contraseña es válida: ${isPasswordRepeatedValid}
+Es mayor de edad: ${isAgeValid}`);
